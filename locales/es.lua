ad58dc3b8a35a5dc3414b98e974e2af92e24874e
o0Coro0o <david_el_pitu@hotmail.com>
2017-09-11 03:21:53 +0200

    Create es.lua
    Added Spanish language

---
+++ locales/es.lua
@@ -0,0 +1,12 @@
+Locales['es'] = {
+
+	['new_message'] = '~b~Nuevo menaje :~s~ %s',
+	['press_take_call'] = ' - Presionar ~INPUT_CONTEXT~ para coger l\'llamada',
+	['taken_call'] = ' Has contestado la l\'llamada',
+	['gps_position'] = 'Posición GPS',
+	['message_sent'] = 'Mensaje enviado',
+	['cannot_add_self'] = 'No has podido agregar',
+	['number_in_contacts'] = 'Este número ya está en tu lista de contactos',
+	['contact_added'] = 'Contacto añadido',
+	['number_not_assigned'] = 'El número n\'no se ha añadido todavía...',
+}
